28ec65c3d608c18af64f302a28db789ab50c0a4a
Angeloamenta <angeloamenta7@gmail.com>
2021-11-18 16:34:48 +0100

    Bonus somma password

--- bonus/js/myscript.js
+++ bonus/js/myscript.js
@@ -7,6 +7,29 @@ Infine scrivi sulla pagina nomecognomecolorepreferito21
 */
 
 
+// // chiedi all'utente il suo nome
+// let nameUser = prompt ("Inserisci il tuo nome");
+// console.log(nameUser);
+// // chiedi all'utente il suo cognome
+// let secondName = prompt ("Inserisci il tuo cognome");
+// console.log(secondName);
+// // chiedi il suo colore preferito
+// let prefColor = prompt ("Inserisci il tuo colore preferito");
+// console.log(prefColor);
+// // costante 21
+// const numberUser = 21;
+// // scrivi sulla pagina nomecognomecolore21
+// document.getElementById("user-name").innerHTML = "<h1>" + nameUser + secondName + prefColor + numberUser + "</h1>";
+
+// // test-user
+// document.getElementById("test-user").innerHTML = document.getElementById("test-user").innerHTML + nameUser + secondName + prefColor + numberUser;
+
+// // test-color
+// document.getElementById("color-user").innerHTML = "<span>" + nameUser + secondName + prefColor + numberUser + "</span>";
+
+// // alert
+// alert("Ciao " + nameUser + secondName + prefColor + numberUser + " grazie per aver effettuato l'accesso");
+
 // chiedi all'utente il suo nome
 let nameUser = prompt ("Inserisci il tuo nome");
 console.log(nameUser);
@@ -16,16 +39,12 @@ console.log(secondName);
 // chiedi il suo colore preferito
 let prefColor = prompt ("Inserisci il tuo colore preferito");
 console.log(prefColor);
-// costante 21
-const numberUser = 21;
+// numero pref
+let prefNumber = prompt("Inserisci il tuo numero preferito");
+console.log(prefNumber);
+// costante 5
+const numberFive = 5;
+// numero
+let numberP = parseInt(prefNumber) + numberFive
 // scrivi sulla pagina nomecognomecolore21
-document.getElementById("user-name").innerHTML = "<h1>" + nameUser + secondName + prefColor + numberUser + "</h1>";
-
-// test-user
-document.getElementById("test-user").innerHTML = document.getElementById("test-user").innerHTML + nameUser + secondName + prefColor + numberUser;
-
-// test-color
-document.getElementById("color-user").innerHTML = "<span>" + nameUser + secondName + prefColor + numberUser + "</span>";
-
-// alert
-alert("Ciao" + nameUser + secondName + prefColor + numberUser);
+document.getElementById("user-name").innerHTML = nameUser + secondName + prefColor + numberP ;
